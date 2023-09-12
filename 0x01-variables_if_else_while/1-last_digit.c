fc6c81f9b77b8e967c7d973af8dee50ea7d9f361
AIRRIK26 <royaleric305@gmail.com>
2023-09-11 17:49:26 -0700

    lastdigits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 
 	if (lastDigit > 5)
 	{
-	printf("greater than 5\n");
+	printf("and is greater than 5\n");
 	}
 	else if (lastDigit == 0)
 	{
@@ -29,7 +29,7 @@ int main(void)
 	}
 	else
 	{
-	printf("less than 6 and not 0\n");
+	printf("and is less than 6 and not 0\n");
 	}
 
 	return (0);
