0b5b25e58d4bdab7050523609ead1cdea3abb43b
AIRRIK26 <royaleric305@gmail.com>
2023-09-11 18:15:03 -0700

    ALPHABETS_putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,16 @@
  *
  * Return: Always 0 
  */
-
-int main() 
+int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++) 
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	putchar(c);
 	}
 
-	for (c = 'A'; c <= 'Z'; c++) 
+	for (c = 'A'; c <= 'Z'; c++)
 	{
 	putchar(c);
 	}
