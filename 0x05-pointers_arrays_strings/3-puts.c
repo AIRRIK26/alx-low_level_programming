e663cef086771a6280a99505d03d8f1843146465
AIRRIK26 <royaleric305@gmail.com>
2023-09-18 14:55:26 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	putchar(*str++);
+	}
+	putchar('\n');
+}
+
